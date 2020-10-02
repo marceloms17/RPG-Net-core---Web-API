e42589485461b4d2f6d36c0c006ecaa49a5dc4b5
Marcelo Morais <marceloms17@gmail.com>
2020-10-02 12:32:41 -0300

    Mais modificaçoes na Controler do Character

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using RPG.Models;
 
@@ -7,11 +9,24 @@ namespace RPG.Controllers
     [Route("[controller]")]
     public class CharacterController: ControllerBase
     {
-        private static Character knight = new Character();
-
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"}
+        };
+        
+        [HttpGet("GetAll")]
         public IActionResult Get()
         {
-            return Ok(knight);
+            return Ok(characters);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+
+        }
+        
     }
 }
